1cdc58993b9410c26bf0422a5eaef15b1198adc7
fred@woodside <frederic.zhang@anu.edu.au>
2022-09-05 11:26:28 +1000

    update arguments
    Signed-off-by: fred@woodside <frederic.zhang@anu.edu.au>

--- image_generation/run.sh
+++ image_generation/run.sh
@@ -1,1 +1,1 @@
-blender --background --python render_images.py -- --num_images 10 --use_gpu 1 --output_image_dir ./output/images --output_scene_dir ./output/scenes --output_scene_file ./output/scenes.json --output_blend_dir ./output/blendfiles --width 256 --height 256
+blender --background --python render_images.py -- --num_images 15000 --use_gpu 1 --output_image_dir ./output/images --output_scene_dir ./output/scenes --output_scene_file ./output/scenes.json --output_blend_dir ./output/blendfiles --width 256 --height 256 --shape_color_combos_json ./combos.json
