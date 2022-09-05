d029eafe95f289c84fafffc581b416f61d133040
fred@woodside <frederic.zhang@anu.edu.au>
2022-09-05 10:49:19 +1000

    add shell script
    Signed-off-by: fred@woodside <frederic.zhang@anu.edu.au>

---
+++ image_generation/run.sh
@@ -0,0 +1,1 @@
+blender --background --python render_images.py -- --num_images 10 --use_gpu 1 --output_image_dir ./output/images --output_scene_dir ./output/scenes --output_scene_file ./output/scenes.json --output_blend_dir ./output/blendfiles --width 256 --height 256
